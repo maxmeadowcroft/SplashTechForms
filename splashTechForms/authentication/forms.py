874964f6dd71bdd68c3204c34b59cfb19d0ada07
max meadowcroft <mmeadowcroft@cc.nmt.edu>
2024-09-30 22:33:07 -0600

    added login and registration

---
+++ splashTechForms/authentication/forms.py
@@ -0,0 +1,21 @@
+# authentication/forms.py
+
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True, label='Email Address')
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+    def __init__(self, *args, **kwargs):
+        super(RegisterForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.add_input(Submit('submit', 'Register'))
--- splashTechForms/authentication/views.py
+++ splashTechForms/authentication/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+# authentication/views.py
 
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import RegisterForm
+
+
+def register_view(request):
+    if request.user.is_authenticated:
+        return redirect('home')  # Redirect authenticated users
+
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}! You can now log in.')
+            return redirect('login')
+    else:
+        form = RegisterForm()
+
+    return render(request, 'authentication/register.html', {'form': form})
